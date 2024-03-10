65b6c5c4f702b77030ca812eb0980b01dd215866
invjhonny <invjhonny04@gmail.com>
2024-03-10 21:22:23 +0900

    blend to react env

--- default_module/today-i-learned/src/App.js
+++ default_module/today-i-learned/src/App.js
@@ -1,6 +1,15 @@
+import './style.css'
 
 function App() {
-  return <h1>Hello World!</h1>
+  return (
+    <header className="header">
+      <div className="logo">
+        <img src="logo.png" width="68" height="68" alt="Today I Learned logo" />
+        <h1>Today I Learned</h1>
+        <button className="btn btn-open btn-large">Share a fact</button>
+      </div>
+    </header>
+  );
 }
 
 export default App;
