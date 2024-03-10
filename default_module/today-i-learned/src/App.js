b7355f4e0a8632a6de7b2c044f6599db93298354
invjhonny <invjhonny04@gmail.com>
2024-03-11 04:08:30 +0900

    touchup css for mobile and scroll

--- default_module/today-i-learned/src/App.js
+++ default_module/today-i-learned/src/App.js
@@ -138,7 +138,7 @@ function isValidHttpUrl(string) {
 
 function NewFactForm({ setFacts, setShowForm }) {
   const [text, setText] = useState("");
-  const [source, setSource] = useState("http://example.com");
+  const [source, setSource] = useState("");
   const [category, setCategory] = useState("");
   const [isUploading, setIsUploading] = useState(false);
   const textLength = text.length;
@@ -239,6 +239,7 @@ function FactList({ facts, setFacts }) {
 
 function Fact({ fact, setFacts }) {
   const [isUpdating, setIsUpdating] = useState(false);
+  const isDisputed = fact.votesInteresting + fact.votesMindblowing < fact.votesFalse;
 
   async function handleVote(columnName) {
     setIsUpdating(true);
@@ -257,6 +258,9 @@ function Fact({ fact, setFacts }) {
   return (
     <li className="fact">
       <p>
+        {isDisputed
+          ? <span className="disputed">[⛔ DISPUTED]</span>
+          : null}
           {fact.text}
           <a href={fact.source} target="_blank" className="source" rel="noreferrer">(Source)</a>
       </p>
